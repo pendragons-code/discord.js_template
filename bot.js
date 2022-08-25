cd89db904755b6ecc67d53076a02661769270eda
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-08-25 23:53:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,36 @@
 const fs = require('fs')
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const Discord = require('discord.js')
-const { Client, Intents } = require('discord.js');
-const bot = new Client({ intents: 32767 });
-bot.commands = new Discord.Collection();
-bot.responses = new Discord.Collection();
+const { Client, GatewayIntentBits, Collection} = require('discord.js');
+const { QuickDB } = require('quick.db');
+const db = new QuickDB();
+const bot = new Client({
+  intents: [
+	GatewayIntentBits.DirectMessages,
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildBans,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+  	GatewayIntentBits.GuildWebhooks,
+  	GatewayIntentBits.DirectMessageReactions,
+  	GatewayIntentBits.DirectMessageTyping,
+  	GatewayIntentBits.GuildEmojisAndStickers,
+	GatewayIntentBits.GuildIntegrations,
+  	GatewayIntentBits.GuildInvites,
+  	GatewayIntentBits.GuildMembers,
+  	GatewayIntentBits.GuildMessageReactions,
+  	GatewayIntentBits.GuildMessageTyping,
+  	GatewayIntentBits.GuildPresences,
+  	GatewayIntentBits.GuildScheduledEvents,
+  	GatewayIntentBits.GuildVoiceStates
+  ]
+});
+bot.commands = new Collection();
+bot.structures = new Collection();
 function commander(){
-console.log(`\x1b[32m%s`, "---------------------------------------------------------------------")
-console.log(`\x1b[32m%s`, "⭕ Commands ⭕")
-console.log(`\x1b[32m%s`, "---------------------------------------------------------------------")
-fs.readdirSync('./commands').forEach(dirs => {
-const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+console.log(`\x1b[32m%s`, "---------------------------------------------------------------------\n⭕ Commands ⭕\n---------------------------------------------------------------------")
+fs.readdirSync('./src/commands').forEach(dirs => {
+const commands = fs.readdirSync(`./src/commands/${dirs}`).filter(files => files.endsWith('.js'));
 for (const file of commands) {
-        const command = require(`./commands/${dirs}/${file}`);
+        const command = require(`./src/commands/${dirs}/${file}`);
         console.log(`\x1b[32m%s`, `Loading command ${file} from ${dirs} ✅!`);
         bot.commands.set(command.name.toLowerCase(), command);
     };
@@ -22,16 +39,29 @@ console.log(`\x1b[32m%s`, "-----------------------------------------------------
 }
 
 function eventer() {
-console.log(`\x1b[35m%s`, "---------------------------------------------------------------------")
-console.log(`\x1b[35m%s`, "⭕ Events ⭕")
-console.log(`\x1b[35m%s`, "---------------------------------------------------------------------")
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of events) {
-    console.log(`\x1b[35m%s`,`Loading discord.js event ${file} ✅!`);
-    const event = require(`./events/${file}`);
-    bot.on(file.split(".")[0], event.bind(null, bot));
-};
+console.log(`\x1b[35m%s`, "---------------------------------------------------------------------\n⭕ Events ⭕\n---------------------------------------------------------------------")
+fs.readdirSync('./events').forEach(dirs => {
+const events = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
+	for (const file of events) {
+    	console.log(`\x1b[35m%s`,`Loading discord.js event ${file} ✅!`);
+    	const event = require(`./events/${dirs}/${file}`);
+    	bot.on(file.split(".")[0], event.bind(null, bot));
+	};
+})
 console.log(`\x1b[35m%s`, "---------------------------------------------------------------------")
 }
 
-module.exports = {eventer: eventer, commander: commander, bot: bot}
+function structurer() {
+console.log('\x1b[36m%s\x1b[0m', `---------------------------------------------------------------------\n⭕ Structures ⭕\n---------------------------------------------------------------------`)
+fs.readdirSync('./src/structure').forEach(dirs => {
+    const structure = fs.readdirSync(`./src/structure/${dirs}`).filter(files => files.endsWith('.js'));
+    for (const file of structure) {
+            const structure = require(`./src/structure/${dirs}/${file}`);
+            console.log('\x1b[36m%s\x1b[0m', `Loading additional component ${file} from ${dirs} ✅!`);
+            bot.structures.set(structure.name.toLowerCase(), structure);
+        };
+      })
+console.log('\x1b[36m%s\x1b[0m', "---------------------------------------------------------------------")
+    }
+
+module.exports = {eventer: eventer, commander: commander, structurer: structurer, bot: bot, db: db}
