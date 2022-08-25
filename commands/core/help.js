d94d073b2d1c60525a6eae2184f621c8435ed522
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-08-25 23:59:12 +0800

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -5,13 +5,13 @@ module.exports = {
     category: 'core',
     utilisation: `help`,
     desc: "Fetches list of commands!",
-execute(bot, messageCreate, args) {
-    const embed = new Discord.MessageEmbed();
+async execute(bot, messageCreate, args) {
+    const embed = new Discord.EmbedBuilder();
     embed.setColor('RED');
     const commands = bot.commands.filter(x => x.showHelp !== false);
     embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(', '));
     embed.setTimestamp();
     embed.setDescription("How to read: `command_name(alias)`!")
-    embed.setFooter({text:'hi', iconURL:messageCreate.author.avatarURL({ dynamic: true })});
+    embed.setFooter({ text: "Star LISBDIE" });
     messageCreate.channel.send({ embeds: [embed] });
 }}
